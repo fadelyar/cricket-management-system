6b688749a05086dff035c27bdaf8b8ea7d3fcc51
Faisal <adelyarfaisal@gmail.com>
2022-02-13 15:56:06 +0430

    insp

--- src/components/user-profile/UserProfile.js
+++ src/components/user-profile/UserProfile.js
@@ -1,6 +1,6 @@
 import React from 'react';
-import {makeStyles} from "@material-ui/core/styles";
-import {styles} from '../../assets/jss/user-profile-style'
+import { makeStyles } from "@material-ui/core/styles";
+import { styles } from '../../assets/jss/user-profile-style'
 import Avatar from "@material-ui/core/Avatar";
 import Typography from "@material-ui/core/Typography";
 
@@ -10,23 +10,20 @@ function UserProfile(props) {
 	const classes = useStyle()
 	return (
 		<div className={classes.root}>
-			<div style={{display: 'flex', justifyContent: 'center'}}>
-				<Avatar src='background_image/logo.jpg' className={classes.avatar}/>
+			<div style={{ display: 'flex', justifyContent: 'center' }}>
+				<Avatar src='background_image/logo.jpg' className={classes.avatar} />
 			</div>
 			<div className={classes.profileBody}>
-				<Typography className={classes.typography} style={{color: 'gray'}}
-							variant='button'>
+				<Typography className={classes.typography} style={{ color: 'gray' }}
+					variant='button'>
 					Admin
 				</Typography>
 				<Typography className={classes.typography} variant='h6'>
 					Faisal Adelyar
 				</Typography>
-				<Typography style={{marginTop: 20, color: 'gray'}} className={classes.typography}>
-					Don't be scared of the truth because we need to restart the human foundation
-					in truth
-					And I love you like Kanye loves Kanye I love Rick Owens’ bed design but the
-					back
-					is...
+				<Typography style={{ marginTop: 20, color: 'gray' }} className={classes.typography}>
+					Put the past behind you, the present moment is all that matters, in the life of
+					those who want to be happy
 				</Typography>
 			</div>
 		</div>
